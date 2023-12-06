921dfa6e25acf4b0159ddc492ac20291bcc66692
Yago Inacio <yagofaran@gmail.com>
2023-12-06 09:49:58 -0300

    skills resizing adjustments

--- src/components/Skill/index.tsx
+++ src/components/Skill/index.tsx
@@ -9,8 +9,6 @@ type Props = {
 }
 
 export function Skill({ directionLeft, tech: { name, src } }: Props) {
-  const MotionImage = motion(Image)
-
   return (
       <motion.div
         initial={{
@@ -26,14 +24,14 @@ export function Skill({ directionLeft, tech: { name, src } }: Props) {
           scale: 1.2,
           transition: { duration: 0.3 },
         }}
-        className="relative group rounded-full border border-brand-gray h-24 w-24 xl:h-32 xl:w-32 inline-block"
+        className="relative group rounded-full border border-brand-gray h-24 w-24 inline-block"
       >
         <Image
           src={`${process.env.NEXT_PUBLIC_PORTFOLIO_API}/images/icons/${src}`}
           alt={name}
           width={500}
           height={500}
-          className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 object-contain h-16 w-16 xl:h-20 xl:w-20"
+          className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 object-contain h-16 w-16"
         />
         <div className="absolute -top-[40%] left-1/2 transform -translate-x-1/2 bg-brand-gray/90 px-2 py-1 rounded
           opacity-0 invisible group-hover:opacity-100 group-hover:visible transition whitespace-nowrap">
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -21,7 +21,7 @@ export function Skills({ skills }: Props) {
         Skills
       </h3>
 
-      <div className="grid grid-cols-4 gap-5">
+      <div className="grid grid-cols-4 md:grid-cols-5 xl:grid-cols-6 gap-5">
         {skills.map((skill, idx) => {
           return (
             <Skill
